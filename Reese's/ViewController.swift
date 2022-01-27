58e9c8cb744ee2de30dbcef454ce7e4d4a473160
Tsukasa Mita <tsukasa.mita@gmail.com>
2022-01-28 00:01:37 +0100

    Reese's Exercise done

--- Reese's/ViewController.swift
+++ Reese's/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        leftLabel.text = ""
+        rightLabel.text = ""
     }
-
+    
+    @IBAction func heyButton(_ sender: UIButton) {
+        if leftLabel.text == "" {
+            leftLabel.text = "You Got Peanut Butter in my Chocolate!"
+            rightLabel.text = ""
+        } else if leftLabel.text == "You Got Peanut Butter in my Chocolate!" {
+            rightLabel.text = "You Got Chocolate in my Peanut Butter!"
+            leftLabel.text = ""
+        }
+    }
+    
+   
+    
 
 }
 
